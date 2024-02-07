9d5b63833bcd23df3580eaeed854298775f58ac9
sheltonsyscorp <shelton.oliveira.barbosa@gmail.com>
2024-02-07 15:35:22 -0300

    create files in folder src and update in all

--- products/models.py
+++ products/models.py
@@ -1,6 +1,6 @@
-
 from django.db import models
-
+from .utils import unique_slug_generator
+from django.db.models.signals import pre_save
 #Custom queryset
 class ProductQuerySet(models.query.QuerySet):
     def active(self):
@@ -38,9 +38,16 @@ class Product(models.Model): #product_category
     active      = models.BooleanField(default = True)
 
     objects = ProductManager()
+    
     #python 3
     def __str__(self):
         return self.title
     #python 2
     def __unicode__(self):
         return self.title
+
+def product_pre_save_receiver(sender, instance, *args, **kwargs):
+    if not instance.slug:
+        instance.slug = unique_slug_generator(instance)
+
+pre_save.connect(product_pre_save_receiver, sender = Product)
